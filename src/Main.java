8e7562d37043e7156c7be49826262c96c09941ab
Esau Hernandez <esauh_24@alu.uabcs.mx>
2026-02-19 12:39:58 -0700

    CambiosCuatro

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import Programacion3.Ventana;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Ventana miventana = new Ventana();
 	
 	}
-
 }
