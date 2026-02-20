d658b7e897ef8cf6f9213afecdd60e434594ec81
Esau Hernandez <esauh_24@alu.uabcs.mx>
2026-02-20 10:50:05 -0700

    Prediseño de LOGIN
    Trabajo de clase

--- src/Programacion3/Ventana.java
+++ src/Programacion3/Ventana.java
@@ -2,23 +2,62 @@ package Programacion3;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
 	
 	public Ventana () {
 		
-		JFrame ventana = new JFrame();
+		this.setVisible(true);
+		this.setSize(500,500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setMinimumSize(new Dimension (200, 200));
+		this.setMaximumSize(new Dimension (1920, 1080));
+		this.setTitle("HOLA");
+		//ventana.setBackground(Color.black);
 		
-		ventana.setVisible(true);
-		ventana.setSize(500,500);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setLocationRelativeTo(null);
-		ventana.setMinimumSize(new Dimension (200, 200));
-		ventana.setMaximumSize(new Dimension (1920, 1080));
-		ventana.setTitle("HOLAHOLA");
-		ventana.setBackground(Color.black);
+		JPanel contenedor = new JPanel();
+		contenedor.setOpaque(true);
+		contenedor.setBackground(Color.black);
+		contenedor.setSize(500,500);
+		contenedor.setLocation(0,0);
+		contenedor.setLayout(null);
+		this.add(contenedor);
+
+		this.add(contenedor);
+
+		JLabel title_login = new JLabel();
+		title_login.setText("BIENVENIDO ");
+		title_login.setSize(200,30);
+		title_login.setOpaque(true);
+		title_login.setLocation(150,10);
+		title_login.setBackground(Color.white);
+		title_login.setFont(new Font ("Arial",Font.BOLD,22));
+		title_login.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(title_login);
+		
+		JTextField username = new JTextField();
+		username.setSize(200,30);
+		username.setLocation(150,50);
+		username.setFont(new Font ("Arial",Font.BOLD,22));
+		contenedor.add(username);
+
+		JButton acceder = new JButton();
+		acceder.setText("ACCEDER");
+		acceder.setLocation(200,400);
+		acceder.setSize(120,50);
+		acceder.setFont(new Font ("Arial",Font.BOLD,22));
+		contenedor.add(acceder);
+	
+		contenedor.repaint();
+		contenedor.revalidate();
 		
 		
 	}
